ca6e5271b0a56d6a8e5e7937d1767c90b5fb2eb0
nitin293 <nitinnlb@gmail.com>
2022-05-16 16:59:14 +0530

    bug fixed for spectogram_generator

--- spectogram_generator.py
+++ spectogram_generator.py
@@ -9,27 +9,36 @@ import argparse
 
 
 def generate_spectogram(csv_filename): 
-  try:
-    aud_dataset = pd.read_csv(csv_filename)
+    try:
+        aud_dataset = pd.read_csv(csv_filename)
 
-    
-    for i in range(len(aud_dataset)):  
-      filename = aud_dataset.iloc[i]["file"]
-      genre = aud_dataset.iloc[i]["genre"]
+        dir = "spectograms"
+        if dir not in os.listdir():
+            os.mkdir(dir)
 
-      img_filename = re.findall(f'[a-zA-Z0-9.]+', filename)[-1]
-      img_filename = f"{img_filename[:-3]}png"
-      output_path = f"./spectograms/{genre}/{img_filename}"
+        genres = aud_dataset["genre"]    
+        for genre in genres:
+            genre_dir = f"{dir}/{genre}"
+            if genre not in os.listdir(dir):
+                os.mkdir(genre_dir)
+                
+        for i in range(len(aud_dataset)):  
+            filename = aud_dataset.iloc[i]["file"]
+            genre = aud_dataset.iloc[i]["genre"]
 
-      samplerate, data = wavfile.read(filename)
-      powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(data, Fs=samplerate)
-    
-      plt.savefig(output_path, frameon='false')
+            img_filename = re.findall(f'[a-zA-Z0-9.]+', filename)[-1]
+            img_filename = f"{img_filename[:-3]}png"
+            output_path = f"./spectograms/{genre}/{img_filename}"
 
-    return True
-    
-  except:
-    return False
+            samplerate, data = wavfile.read(filename)
+            powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(data, Fs=samplerate)
+                
+            plt.savefig(output_path, frameon='false')
+
+        return True
+            
+    except:
+        raise
 
 
 if __name__=="__main__":
