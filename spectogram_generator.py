089a7d7d6f91ed823fa868801ceebdf361099106
nitin293 <nitinnlb@gmail.com>
2022-05-16 19:00:00 +0530

    multi threaded

--- spectogram_generator.py
+++ spectogram_generator.py
@@ -5,10 +5,21 @@ import scipy
 import scipy.io.wavfile as wavfile
 import librosa
 import matplotlib.pyplot as plt
+import threading
 import argparse
 
 
-def generate_spectogram(csv_filename): 
+def generate_spectogram(wavfile, outfile):
+    rate, data = wavfile.read(wavfile)
+    fig,ax = plt.subplots(1)
+    fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
+    ax.axis('off')
+    pxx, freqs, bins, im = ax.specgram(x=data, Fs=rate, noverlap=384, NFFT=512)
+    ax.axis('off')
+    fig.savefig(outfile, dpi=300, frameon='false')
+
+
+def runner(csv_filename, threads):
     try:
         aud_dataset = pd.read_csv(csv_filename)
 
@@ -27,14 +38,17 @@ def generate_spectogram(csv_filename):
             genre = aud_dataset.iloc[i]["genre"]
 
             img_filename = re.findall(f'[a-zA-Z0-9.]+', filename)[-1]
-            img_filename = f"{img_filename[:-3]}png"
-            output_path = f"./spectograms/{genre}/{img_filename}"
+            img_filename = f"{img_filename[:-4]}.png"
+            output_file = f"./spectograms/{genre}/{img_filename}"
 
-            samplerate, data = wavfile.read(filename)
-            powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(data, Fs=samplerate)
-                
-            plt.savefig(output_path, frameon='false')
+            thread = threading.Thread(target=generate_spectogram, args=(filename, output_file, ))
+            thread.start()
 
+            if i%threads==0:
+                thread.join()
+            elif i==len(aud_dataset):
+                thread.join()
+            
         return True
             
     except:
@@ -45,10 +59,19 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-f", "--file",
+        type=str,
         help="Mapped CSV filename",
         required=True
+    )
+    parser.add_argument(
+        "-t", "--thread",
+        type=int,
+        help="Threads",
+        default=1
     )
     args = parser.parse_args()
 
     csv_file = args.file
-    generate_spectogram(csv_file)
+    thread = args.thread
+
+    runner(csv_filename=csv_file, threads=thread)
